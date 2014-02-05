dabb48d2c81d29c2ce098f27410cc28dd762cdef
James Nutt <james.srn@gmail.com>
2014-02-05 23:17:43 +0000

    zip breaker

---
+++ zip_cracker/unzipper.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'archive/zip'
+
+class Unzipper
+  def initialize
+    puts "*" * 80
+    puts "* zip file brute forcer"
+    puts "* @jsrn"
+    puts "*" * 80
+
+    @infile = ARGV[0]
+    @dict   = ARGV[1]
+
+    @infile ||= "secret.zip"
+    @dict   ||= "../shared/dict.txt"
+
+    crack
+  end
+
+  def crack
+    puts "[+] beginning brute force of #{@infile}"
+    File.open(@dict).read.each_line do |pass|
+      pass.strip!
+      begin
+        Archive::Zip.extract( @infile, "out/", :password => pass )
+        puts " -  PASSWORD FOUND: #{pass}"
+        exit
+      rescue
+      end
+    end
+  end
+end
+
+unzipper = Unzipper.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
